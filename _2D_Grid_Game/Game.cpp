a6a1cc1bf057b6f16c40f3dd60a1967abe04ab34
Woeloe <Sennewolle@hotmail.com>
2024-12-06 09:56:02 +0100

    first commit

---
+++ _2D_Grid_Game/Game.cpp
@@ -0,0 +1,554 @@
+#include "pch.h"
+#include "Game.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+//Basic game functions
+#pragma region gameFunctions											
+void Start()
+{
+	// initialize game resources here
+
+	LoadSprites();
+	ResetGame();
+
+}
+
+void Draw()
+{
+	ClearBackground(0.5f,0.5f,0.5f);
+	DrawGrid(g_Grid, g_NrRows, g_NrCols, g_CellSize, Point2f(0.f, 0.f));
+	
+	if (g_Tank1.isAlive)
+	{
+		DrawTank(g_Tank1);
+	}
+	if (g_Tank2.isAlive)
+	{
+		DrawTank(g_Tank2);
+	}
+
+	if (!g_Tank1.canShoot)
+	{
+		DrawBullet(g_Tank1);
+	}
+	if (!g_Tank2.canShoot)
+	{
+		DrawBullet(g_Tank2);
+	}
+}
+
+void Update(float elapsedSec)
+{
+	const Uint8 *pStates = SDL_GetKeyboardState( nullptr );
+	MoveTank(g_Tank1, pStates, elapsedSec);
+	MoveTank(g_Tank2, pStates, elapsedSec);
+	
+	if (!g_Tank1.canShoot)
+	{
+		UpdateBullet(g_Tank1, elapsedSec);
+	}
+
+	if (!g_Tank2.canShoot)
+	{
+		UpdateBullet(g_Tank2, elapsedSec);
+	}
+}
+
+void End()
+{
+	// free game resources here
+	for (int i{}; int(g_NrCols * g_NrRows) > i; i++)
+	{
+		if (g_PArrCells[i] != nullptr)
+		{
+		DeleteTexture(*g_PArrCells[i]);
+		}
+	}
+}
+#pragma endregion gameFunctions
+
+//Keyboard and mouse input handling
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key)
+{
+
+}
+
+void OnKeyUpEvent(SDL_Keycode key)
+{
+	if (key == g_Tank1.shootKey)
+	{
+		FireBullet(g_Tank1);
+	}
+	if (key == g_Tank2.shootKey)
+	{
+		FireBullet(g_Tank2);
+	}
+	if (g_IsGameOver && key == SDLK_r)
+	{
+		ResetGame();
+	}
+}
+
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e)
+{
+	// SAMPLE CODE: print mouse position
+	//const float mouseX{ float(e.x) };
+	//const float mouseY{ float(e.y) };
+	//std::cout << "  [" << mouseX << ", " << mouseY << "]\n";
+}
+
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e)
+{
+	// SAMPLE CODE: print mouse position
+	//const float mouseX{ float(e.x) };
+	//const float mouseY{ float(e.y) };
+	//std::cout << "  [" << mouseX << ", " << mouseY << "]\n";
+}
+
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e)
+{
+	// SAMPLE CODE: print mouse position
+	//const float mouseX{ float(e.x) };
+	//const float mouseY{ float(e.y) };
+	//std::cout << "  [" << mouseX << ", " << mouseY << "]\n";
+	
+	// SAMPLE CODE: check which mouse button was pressed
+	//switch (e.button)
+	//{
+	//case SDL_BUTTON_LEFT:
+	//	//std::cout << "Left mouse button released\n";
+	//	break;
+	//case SDL_BUTTON_RIGHT:
+	//	//std::cout << "Right mouse button released\n";
+	//	break;
+	//case SDL_BUTTON_MIDDLE:
+	//	//std::cout << "Middle mouse button released\n";
+	//	break;
+	//}
+}
+#pragma endregion inputHandling
+
+#pragma region ownDefinitions
+// Define your own functions here
+void MoveTank(Tank& tank, const Uint8* pStates, float elapsedSec)
+{
+	if (!tank.isAlive)
+	{
+		return;
+	}
+
+	const float moveSpeed{ 200.f };
+
+	Vector2f directionVector{};
+	Direction direction{Direction::STATE_COUNT};
+
+	//TODO direction veranderen met vector ipv per input
+	if (pStates[tank.movementKeys[0]])
+	{
+		++directionVector.y;
+		direction = Direction::down;
+	}
+	if (pStates[tank.movementKeys[1]])
+	{
+		--directionVector.y;
+		direction = Direction::up;
+	}
+	if (pStates[tank.movementKeys[2]])
+	{
+		--directionVector.x;
+		direction = Direction::left;
+	}
+	if (pStates[tank.movementKeys[3]])
+	{
+		++directionVector.x;
+		direction = Direction::right;
+	}
+
+	//Only normalize vector when there is input
+	if (Length(directionVector))
+	{
+		directionVector = Normalize(directionVector);
+	}
+
+	Point2f delta{
+		directionVector.x * moveSpeed * elapsedSec,
+		directionVector.y * moveSpeed * elapsedSec
+	};
+
+	Rectf playerHitbox{ tank.GetHitbox()};
+	playerHitbox.left += delta.x;
+	playerHitbox.top += delta.y;
+
+	if (IsOverlapping(playerHitbox, getEnemyPointer(tank)->GetHitbox()))
+	{
+		return;
+	}
+
+	bool isHorizontalMovementAllowed{ IsHitboxMovementAllowed(Rectf(tank.position.x + delta.x, tank.position.y, tank.sprite.spriteSize, tank.sprite.spriteSize)) };
+	bool isVerticalMovementAllowed{ IsHitboxMovementAllowed(Rectf(tank.position.x, tank.position.y + delta.y , tank.sprite.spriteSize, tank.sprite.spriteSize)) };
+
+	if (isHorizontalMovementAllowed)
+	{
+		tank.position.x += delta.x;
+	}
+	else if (directionVector.y)
+	{
+		if (signbit(directionVector.y))
+		{
+			direction = Direction::up;
+		}
+		else 
+		{
+			direction = Direction::down;
+		}
+	}
+
+	if (isVerticalMovementAllowed)
+	{
+		tank.position.y += delta.y;
+	}
+
+	if (direction != Direction::STATE_COUNT)
+	{
+		tank.direction = direction;
+	}
+}
+
+void DrawGrid(int grid[], int nrRows, int nrCols, int cellSize, Point2f drawPos, float scale)
+{
+	const float startX{ drawPos.x };
+
+
+
+	SetColor(0.5f, 0.5f, 0.5f, 1.f);
+	for (int row{}; row < nrRows; ++row)
+	{
+		for (int col{}; col < nrCols; ++col)
+		{
+
+			Texture* pTexture{ g_PArrCells[GetIndex(row, col, nrCols)] };
+			if (pTexture != nullptr)
+			{
+				Rectf destinationRect{ drawPos.x, drawPos.y, g_CellSize, g_CellSize };
+				DrawTexture(*(g_PArrCells[GetIndex(row, col, nrCols)]), destinationRect);
+				
+			}
+			// DrawRect(drawPos, float(cellSize), float(cellSize));
+			drawPos.x += cellSize;
+		}
+
+		drawPos.x = startX;
+		drawPos.y += cellSize;
+	}
+}
+
+void DrawTank(const Tank& tank)
+{
+	SetColor(1.f, 0.f, 0.f, 1.f);
+	const float tankSize{ 20.f };
+
+	const float spriteSize{ tank.sprite.spriteSize };
+	const float scale{ 3.f / 4.f };
+
+	//Calc offset from center with texture dimensions:
+	Rectf sourceRect{ int(tank.direction) * tank.sprite.spriteSize, 0, tank.sprite.spriteSize, tank.sprite.spriteSize };
+	Rectf destinationRect{ tank.position.x, tank.position.y, tank.sprite.spriteSize, tank.sprite.spriteSize};
+
+	//Draw tank sprite
+	DrawTexture(tank.sprite.texture, destinationRect, sourceRect);
+	FillEllipse(tank.position, 5, 5);
+
+}
+
+int GetIndex(int rowIndex, int columnIndex, int numColumns)
+{
+	int index{ numColumns * rowIndex + columnIndex };
+	return  index;
+}
+int GetRow(int index, int numCols)
+{
+	int row{  };
+	if (index < numCols)
+	{
+		row = 0;
+	}
+	else
+	{
+		row = index / numCols;
+	}
+
+	return row;
+}
+int GetCol(int index, int numCols)
+{
+	int col{ index % numCols };
+	return col;
+}
+
+int PointToGridIndex(Texture **pointerArr, int rows, int cols, int cellSize, const Point2f& gridPos, const Point2f& point)
+{
+	const Point2f delta{ point.x - gridPos.x, point.y - gridPos.y };
+	if (delta.x < 0 || delta.x > cellSize * cols)
+	{
+		return  -1;
+	}
+	if (delta.y < 0 || delta.y > cellSize * rows)
+	{
+		return -1;
+	}
+
+	const int colIndex{ int(delta.x) / cellSize };
+	const int rowIndex{ int(delta.y) / cellSize };
+	int tankIndex{ rowIndex * cols + colIndex }; // = tankindex
+
+	return tankIndex;
+}
+int PointToGridIndex(const Point2f& point)
+{
+	return PointToGridIndex(g_PArrCells, g_NrRows, g_NrCols, g_CellSize, Point2f(0, 0), point);
+}
+bool IsMovementAllowed(int gridIndex)
+{
+	return g_PArrCells[gridIndex] == nullptr;
+}
+
+bool IsHitboxMovementAllowed(const Rectf& rect)
+{
+	const Point2f points[]{
+		Point2f(rect.left, rect.top),
+		Point2f(rect.left + rect.width, rect.top),
+		Point2f(rect.left, rect.top + rect.height),
+		Point2f(rect.left + rect.width, rect.top + rect.height),
+		Point2f(rect.left + rect.width / 2, rect.top),
+		Point2f(rect.left + rect.width / 2, rect.top + rect.height),
+		Point2f(rect.left, rect.top + rect.height / 2),
+		Point2f(rect.left + rect.width, rect.top + rect.height / 2)
+	};
+
+	bool isMovementAllowed{ true };
+	for (int idx{}; idx < sizeof(points) / sizeof(points[0]); ++idx)
+	{
+		int gridIndex{ PointToGridIndex(points[idx]) };
+		isMovementAllowed = isMovementAllowed && IsMovementAllowed(gridIndex);
+	}
+
+	return isMovementAllowed;
+}
+
+Tank* getEnemyPointer(const Tank& tank)
+{
+	if (tank.id == 1)
+	{
+		return &g_Tank2;
+	}
+	else
+	{
+		return &g_Tank1;
+	}
+}
+
+void DrawBullet(const Tank& tank)
+{
+	const float radius{ 10.f };
+	utils::FillEllipse(tank.bullet.position, radius, radius);
+}
+
+void UpdateBullet(Tank& tank, float elapsedSec)
+{
+	const float bulletSpeed{300.f};
+	
+	Vector2f directionVectors[]{
+		Vector2f{0.f, -1.f},
+		Vector2f{1.f, 0.f},
+		Vector2f{0.f, 1.f},
+		Vector2f{-1.f, 0.f}
+	};
+
+	Vector2f direction{ directionVectors[int(tank.bullet.direction)] };
+	tank.bullet.position.x += direction.x * bulletSpeed * elapsedSec;
+	tank.bullet.position.y += direction.y * bulletSpeed * elapsedSec;
+
+	CheckBulletConditions(tank);
+}
+void CheckBulletConditions(Tank& tank)
+{
+	//is bullet out of bounds?
+	Rectf window{ 0, 0, g_WindowWidth, g_WindowHeight };
+	if (!IsPointInRect(tank.bullet.position, window))
+	{
+		tank.canShoot = true;
+		return;
+	}
+
+	//does bullet hit a wall?
+	int bulletIndex{ PointToGridIndex(g_PArrCells, g_NrRows, g_NrCols, g_CellSize, Point2f(0,0), tank.bullet.position) };
+
+	if (!IsMovementAllowed(bulletIndex))
+	{
+		tank.canShoot = true;
+		DestroyWall(bulletIndex, tank.bullet.direction);
+		return;
+	}
+
+	//Did bullet hit the enemy
+	Tank* pEnemy{getEnemyPointer(tank)};
+
+	bool isEnemyHit{ IsPointInRect(tank.bullet.position, pEnemy->GetHitbox()) };
+	if (isEnemyHit && pEnemy->isAlive)
+	{
+		tank.canShoot = true;
+		HitEnemy(*pEnemy);
+		return;
+	}
+
+	//Does bullet collide with enemy's bullet?
+	if (IsOverlapping(Circlef(tank.bullet.position, 5.f), Circlef(pEnemy->bullet.position, 5.f)))
+	{
+		tank.canShoot = true;
+		pEnemy->canShoot = true;
+	}
+
+}
+void FireBullet(Tank& tank)
+{
+	if (tank.isAlive && tank.canShoot)
+	{
+		tank.bullet.position = tank.GetCenterPos();
+		tank.bullet.direction = tank.direction;
+		tank.canShoot = false;
+	}
+}
+void HitEnemy(Tank& enemy)
+{
+	--enemy.health;
+
+	if(!enemy.health)
+	{
+		enemy.isAlive = false;
+		g_IsGameOver = true;
+	}
+}
+void DestroyWall(int gridIndex, Direction bulletDirection)
+{
+	g_PArrCells[gridIndex] = nullptr;
+
+		if (bulletDirection == Direction::up || bulletDirection == Direction::down)
+		{
+			if (gridIndex - 1 >= 0)
+			{
+				g_PArrCells[gridIndex - 1] = nullptr;
+			}
+			if (gridIndex + 1 < g_NrRows * g_NrCols)
+			{
+				g_PArrCells[gridIndex + 1] = nullptr;
+			}
+		}
+		else
+		{
+			if (gridIndex - g_NrCols >= 0)
+			{
+				g_PArrCells[gridIndex - g_NrCols] = nullptr;
+			}
+			if (gridIndex + g_NrCols < g_NrRows * g_NrCols)
+			{
+				g_PArrCells[gridIndex + g_NrCols] = nullptr;
+			}
+		}
+}
+#pragma endregion ownDefinitions
+
+Point2f Tank::GetCenterPos()
+{
+	return Point2f(position.x + sprite.spriteSize / 2.f, position.y + sprite.spriteSize / 2.f);
+}
+
+Rectf Tank::GetHitbox()
+{
+	return Rectf(position.x, position.y, sprite.spriteSize, sprite.spriteSize);
+}
+
+void ResetGame()
+{
+	InitializeMap();
+
+	g_Tank1.canShoot = true;
+	g_Tank1.health = g_MaxHealth;
+	g_Tank1.isAlive = true;
+
+	g_Tank2.canShoot = true;
+	g_Tank2.health = g_MaxHealth;
+	g_Tank2.isAlive = true;
+}
+
+void LoadSprites()
+{
+	TextureFromFile("resources/Red_Brick.png", g_Muur);
+
+	if (!TextureFromFile("resources/Tank_Two-Sheet.png", g_Tank1.sprite.texture))
+	{
+		std::cout << "Error: loading tank 1 sprite sheet FAILED\n";
+	}
+	else {
+		g_Tank1.sprite.nrCols = 4;
+		g_Tank1.sprite.spriteSize = g_Tank1.sprite.texture.width / g_Tank1.sprite.nrCols;
+	}
+	if (!TextureFromFile("resources/mech_2-Sheet.png", g_Tank2.sprite.texture))
+	{
+		std::cout << "Error: loading tank 2 sprite sheet FAILED\n";
+	}
+	else {
+		g_Tank2.sprite.nrCols = 4;
+		g_Tank2.sprite.spriteSize = g_Tank2.sprite.texture.width / g_Tank2.sprite.nrCols;
+	}
+
+}
+
+void InitializeMap()
+{
+	std::ifstream file("../resources/map.txt");
+	std::string line;
+
+	if (file.is_open())
+	{
+		int row{};
+		while (std::getline(file, line))
+		{
+			for (int idx{}; idx < g_NrCols; ++idx)
+			{
+				if (line[idx] == '#')
+				{
+					g_PArrCells[GetIndex(row, idx, g_NrCols)] = &g_Muur;
+				}
+				else
+				{
+					g_PArrCells[GetIndex(row, idx, g_NrCols)] = nullptr;
+
+					if (line[idx] == '1')
+					{
+						g_Tank1.position.x = float(idx * g_CellSize);
+						g_Tank1.position.y = float(row * g_CellSize);
+					}
+					else if (line[idx] == '2')
+					{
+						g_Tank2.position.x = float(idx * g_CellSize);
+						g_Tank2.position.y = float(row * g_CellSize);
+
+						std::cout << g_Tank2.position.x << "    " << g_Tank2.position.y;
+					}
+				}
+			}
+
+			++row;
+		}
+		file.close();
+	}
+	else
+	{
+		std::cout << "Error opening map\n\n";
+	}
+
+
+}
---
+++ _2D_Grid_Game/Game.h
@@ -0,0 +1,136 @@
+#pragma once
+#include <utils.h>
+using namespace utils;
+
+#pragma region gameInformation
+// TODO: Set your name and group in the title here
+std::string g_WindowTitle{ "Project name - Name, firstname - 1DAExx" };
+
+// Change the window dimensions here
+float g_WindowWidth{ 1280 };
+float g_WindowHeight{ 720 };
+#pragma endregion gameInformation
+
+
+
+#pragma region ownDeclarations
+// Declare your own global variables here
+bool g_IsGameOver{};
+
+const int g_CellSize{ 32 };
+const int g_NrRows{ 24 }, g_NrCols{ 41 };
+int g_Grid[g_NrRows * g_NrCols]{};
+const int g_MaxHealth{ 5 };
+
+Texture g_Muur{};
+Texture * g_PArrCells[g_NrCols * g_NrRows]{};
+
+enum class Direction
+{
+	//Enum also used in array indexing so don't change order;
+	up,
+	right,
+	down,
+	left,
+	STATE_COUNT //Amount of options in enum, also used as checker in MoveTank function
+};
+
+struct Sprite
+{
+	Texture texture;
+	int nrCols;
+	float spriteSize;
+};
+
+struct Bullet {
+	Point2f position;
+	Direction direction;
+};
+
+struct Tank
+{
+	const int id;
+	Point2f position; // position of tank (topleft of the sprite)
+	Direction direction;
+	Sprite sprite;
+	Bullet bullet;
+	bool canShoot;
+	int health;
+	bool isAlive;
+	SDL_Scancode movementKeys[4]; //Down, Up, Left, Right
+	SDL_KeyCode shootKey;
+
+	Point2f GetCenterPos();
+	Rectf GetHitbox();
+};
+
+Tank g_Tank1{
+	1,
+	Point2f{},
+	Direction{},
+	Sprite{},
+	Bullet{},
+	true,
+	g_MaxHealth,
+	true,
+	{SDL_SCANCODE_DOWN, SDL_SCANCODE_UP, SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT},
+	SDLK_RSHIFT
+
+};
+
+Tank g_Tank2{
+	2,
+	Point2f{},
+	Direction{},
+	Sprite{},
+	Bullet{},
+	true,
+	g_MaxHealth,
+	true,
+	{SDL_SCANCODE_S, SDL_SCANCODE_W, SDL_SCANCODE_A, SDL_SCANCODE_D},
+	SDLK_LSHIFT
+};
+
+// Declare your own functions here
+void ResetGame();
+void LoadSprites();
+void InitializeMap();
+
+void MoveTank(Tank& tank, const Uint8* pStates, float elapsedSec);
+void DrawGrid(int grid[], int nrRows, int nrCols, int cellSize, Point2f drawPos, float scale = 1.f);
+void DrawTank(const Tank& tank);
+
+int GetIndex(int rowIndex, int columnIndex, int numCols);
+int GetRow(int index, int numCols);
+int GetCol(int index, int numCols);
+
+int PointToGridIndex(Texture** pointerArr, int rows, int cols, int cellSize, const Point2f& gridPos, const Point2f& point);
+int PointToGridIndex(const Point2f& point);
+bool IsMovementAllowed(int gridIndex);
+bool IsHitboxMovementAllowed(const Rectf& rect);
+Tank* getEnemyPointer(const Tank& tank);
+
+void DrawBullet(const Tank& tank);
+void UpdateBullet(Tank& tank, float elapsedSec);
+void CheckBulletConditions(Tank& tank);
+void FireBullet(Tank& tank);
+void HitEnemy(Tank& enemy);
+void DestroyWall(int gridIndex, Direction bulletDirection);
+
+
+#pragma endregion ownDeclarations
+
+#pragma region gameFunctions											
+void Start();
+void Draw();
+void Update(float elapsedSec);
+void End();
+#pragma endregion gameFunctions
+
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key);
+void OnKeyUpEvent(SDL_Keycode key);
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e);
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e);
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e);
+#pragma endregion inputHandling
